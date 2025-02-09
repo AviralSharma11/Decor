6fe9a610fdc4042131092b72aecde075e17d1d06
Aviral Sharma <aviral0201sharma@gmail.com>
2025-02-09 15:25:15 +0530

    Carousel Done

--- my-app/src/Components/Collections/Material.jsx
+++ my-app/src/Components/Collections/Material.jsx
@@ -124,60 +124,58 @@ export default function Material(){
             
                 {/* Categories Section */}
                 <div className="slider-wave">
-                    <svg
-                      version="1.1"
-                      id="Layer_1"
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      viewBox="0 0 804 50.167"
-                      preserveAspectRatio="none"
-                      xmlSpace="preserve"
-                    >
-                      <path
-                        fill="#333"
-                        d="M804,0v16.671c0,0-204.974,33.496-401.995,33.496C204.974,50.167,0,16.671,0,16.671V0H804z"
-                      />
-                    </svg>
-                  </div>
-                <Splide
-                  options={{
-                    type: "loop",
-                    perPage: 4,
-                    focus: "center",
-                    autoplay: true,
-                    interval: 3000,
-                    pauseOnHover: false,
-                    pagination: false,
-                    arrows: true,
-                    breakpoints: {
-                      768: { perPage: 1 },
-                    },
-                  }}
-                >
-                  {material.map((category, index) => (
-                    <SplideSlide key={index}>
-                      <Link to={category.link}>
-                      <img src={category.image} alt={`Slide ${index + 1}`} className="curved-image" />
-                      <p className="category-name">{category.name}</p>
-                      </Link>
-                    </SplideSlide>
-                  ))}
-                </Splide>
-                <div className="slider-wave2 bottom">
-                <svg
-                  version="1.1"
-                  id="Layer_1" 
-                  xmlns="http://www.w3.org/2000/svg" 
-                  xmlnsXlink="http://www.w3.org/1999/xlink" 
-                  x="0px" 
-                  y="0px" 
-                  viewBox="0 0 804 50.167" 
-                  enableBackground="new 0 0 804 50.167" 
-                  xmlSpace="preserve"
-                >
-                  <path fill="#333" d="M804,0v16.671c0,0-204.974,33.496-401.995,33.496C204.974,50.167,0,16.671,0,16.671V0H804z"></path>
-                </svg>
-                </div>
+                        <svg
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 804 50.167"
+                          preserveAspectRatio="none"
+                        >
+                          <path fill="#333" d="M804,0v16.671c0,0-204.974,33.496-401.995,33.496C204.974,50.167,0,16.671,0,16.671V0H804z"/>
+                        </svg>
+                      </div>
+                
+                      {/* Splide Carousel */}
+                      <div className="sliders">
+                      <Splide
+                        className="screen"
+                        options={{
+                          type: "loop",
+                          perPage: 4,
+                          focus: "center",
+                          autoplay: true,
+                          interval: 3000,
+                          pauseOnHover: false,
+                          pagination: false,
+                          arrows: true,
+                          breakpoints: {
+                            1024: { perPage: 3 },
+                            768: { perPage: 2 },
+                            480: { perPage: 1 },
+                          },
+                        }}
+                      >
+                        {material.map((category, index) => (
+                          <SplideSlide key={index}>
+                            <Link to={category.link} className="slide">
+                              <img src={category.image} alt={`Slide ${index + 1}`} className="curved-image" />
+                              <p className="category-name">{category.name}</p>
+                            </Link>
+                          </SplideSlide>
+                        ))}
+                      </Splide>
+                
+                      {/* Lower wave */}
+                      <div className="slider-wave2">
+                        <svg
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 804 50.167"
+                          preserveAspectRatio="none"
+                        >
+                          <path fill="#333" d="M804,0v16.671c0,0-204.974,33.496-401.995,33.496C204.974,50.167,0,16.671,0,16.671V0H804z"/>
+                        </svg>
+                      </div>
+                      </div>
             </div>
             {isMobile && (
         <div className="mobile-controls">
